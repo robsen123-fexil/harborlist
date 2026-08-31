55102ef9893b6aa8b0d41061276b790de795a306
Robsen Teshome <robsenteshoma1@gmail.com>
2026-08-31 23:41:28 +0300

    Add App component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { MooringBoard } from '@/components/MooringBoard';
+import './index.css';
+
+export function App() {
+  return (
+    <main className="app">
+      <MooringBoard />
+    </main>
+  );
+}
